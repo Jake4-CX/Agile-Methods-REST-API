c35d75f2a764b457f65f6d01259ad63e6af32baf
Jake4 <jake44264@hotmail.co.uk>
2023-02-12 16:49:49 +0000

    Database option added to env

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,10 @@
 require('dotenv').config();
 
 export const port = process.env.PORT || 3000;
+
+export const database_type = process.env.DATABASE_TYPE || "mysql";
+export const database_address = process.env.DATABASE_ADDRESS || "127.0.0.1";
+export const database_port:number = parseInt(process.env.DATABASE_PORT) || 3306;
+export const database_name = process.env.DATABASE_NAME || "reports";
+export const database_user = process.env.DATABASE_USER || "council_user";
+export const database_password = process.env.DATABASE_NAME || "4*CMgmdI6uv@";
