9029b68c2a8948ffae9371279ca722cb5c3b437b
= <jake44264@hotmail.co.uk>
2023-03-13 16:24:52 +0000

    Additions to voting endpoints

--- src/controller/ReportVoteController.ts
+++ src/controller/ReportVoteController.ts
@@ -11,14 +11,15 @@ export class ReportVoteController {
   private reportRepository = AppDataSource.getRepository(Reports)
 
   async create_report_vote(request: Request, response: Response, next: NextFunction) {
-    let { report_uuid, vote_type } = request.body;
+    const report_uuid = request.params.report_uuid
+    let { vote_type } = request.body;
 
     const user_id: number = request.user_data.id
 
     let report = await this.reportRepository.findOneBy({ report_uuid: report_uuid })
     if (!report) return next(createError(401, "Report does not exist"));
 
-    const report_vote_check = await this.reportVoteRepository.findOneBy({ report: {id: report.id }, user: user_id })
+    const report_vote_check = await this.reportVoteRepository.findOneBy({ report: { id: report.id }, user: { id: user_id } })
 
     if (report_vote_check) return next(createError(401, "The user has already voted on this report"));
 
@@ -32,7 +33,7 @@ export class ReportVoteController {
 
     if (!report_vote) return next(createError(401, "Failed to vote on report")); // report vote couldn't be created
 
-    return {message: "This vote has been created successfully"};
+    return { message: "This vote has been created successfully" };
   }
 
   async delete_report_vote(request: Request, response: Response, next: NextFunction) {
@@ -43,13 +44,13 @@ export class ReportVoteController {
     let report = await this.reportRepository.findOneBy({ report_uuid: report_uuid })
     if (!report) return next(createError(401, "Report does not exist"));
 
-    let report_vote = await this.reportVoteRepository.findOneBy({ report: {id: report.id }, user: user_id })
+    let report_vote = await this.reportVoteRepository.findOneBy({ report: { id: report.id }, user: { id: user_id } })
 
     if (!report_vote) return next(createError(401, "report vote does not exist"));
 
     const delete_vote = await this.reportVoteRepository.remove(report_vote)
 
-    return delete_vote ? {message: "This vote has been deleted successfully"} : next(createError(401, "Failed to delete report vote"));
+    return delete_vote ? { message: "This vote has been deleted successfully" } : next(createError(401, "Failed to delete report vote"));
   }
 
   async update_report_vote(request: Request, response: Response, next: NextFunction) {
@@ -61,13 +62,13 @@ export class ReportVoteController {
     let report = await this.reportRepository.findOneBy({ report_uuid: report_uuid })
     if (!report) return next(createError(401, "Report does not exist"));
 
-    let report_vote = await this.reportVoteRepository.findOneBy({ report: {id: report.id }, user: user_id })
+    let report_vote = await this.reportVoteRepository.findOneBy({ report: { id: report.id }, user: { id: user_id } })
 
     if (!report_vote) return next(createError(401, "report vote does not exist"));
 
     const update_vote = await this.reportVoteRepository.update(report_vote, { vote_type: (vote_type === "upvote" ? 1 : -1) })
 
-    return update_vote ? {message: "This vote has been updated successfully"} : next(createError(401, "Failed to update report vote"));
+    return update_vote ? { message: "This vote has been updated successfully" } : next(createError(401, "Failed to update report vote"));
   }
 
   async get_report_votes(request: Request, response: Response, next: NextFunction) {
@@ -80,15 +81,20 @@ export class ReportVoteController {
     if (!report) return next(createError(401, "Report does not exist"));
 
     let report_votes = await this.reportVoteRepository.createQueryBuilder('report_votes')
-      .where({report: {id: report.id}})
+      .where({ report: { id: report.id } })
       .take(15)
       .select(["report_votes.id", "report_votes.vote_date", "report_votes.vote_type", "user.id", "user.first_name"])
       .leftJoin("report_votes.user", "user")
       .getMany()
-    
+
 
     const has_user_voted: boolean = report_votes.filter(report_vote => (report_vote.user as Users).id === user_id).length > 0
 
-    return report_votes ? { votes: report_votes, user_voted: has_user_voted } : next(createError(401, "Failed to get report votes"));
+    const vote_types = {
+      upvotes: report_votes.filter((report_vote) => report_vote.vote_type == 1).length,
+      downvotes: report_votes.filter((report_vote) => report_vote.vote_type == -1).length
+    }
+
+    return report_votes ? { votes: report_votes, user_voted: has_user_voted, report_votes: vote_types } : next(createError(401, "Failed to get report votes"));
   }
 }
